08105667c5906bc1fe3475995ecb6b70bcd89e48
orlasiimwe <orlanerys1@gmail.com>
2021-02-27 17:17:51 +0300

    homepage edits

--- src/components/ProductGroup/index.jsx
+++ src/components/ProductGroup/index.jsx
@@ -19,14 +19,7 @@ import './styles.css';
 
 const ProductGroup = ({ products, loading }) => {
   const [currentTab, setCurrentTab] = useState('phones');
-
-  // const imgstyles = {
-  //   borderRadius: "50%",
-  //   display: "flex",
-  //   justifyContent: "center",
-  //   width: "100%",
-  //   height: "100%"
-  // }
+  
   const divstyles = {
     fontSize: "10px"
   }
