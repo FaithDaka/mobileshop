e37db19b7a9734f383da0d66811126427606f7ad
orlasiimwe <orlanerys1@gmail.com>
2021-03-26 16:46:18 +0300

    spinner

--- src/components/Spinner/index.js
+++ src/components/Spinner/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const LoadSpinner = () => (
-  <div className="d-flex justify-content-center pt-2 pb-2 mt-2 mb-2">
+  <div className="d-flex justify-content-center pt-2 mt-2 mb-2">
     <div class="loader-demo-box">
       <div class="jumping-dots-loader"> <span></span> <span></span> <span></span> </div>
     </div>
