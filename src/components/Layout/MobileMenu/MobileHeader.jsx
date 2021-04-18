4260e16d3f1f2fa88e0db655b6ef09f9ccbd2941
Frank Mwesigwa <frank.mwesigwa1@gmail.com>
2021-04-18 10:31:27 +0300

    change phone numbers

--- src/components/Layout/MobileMenu/MobileHeader.jsx
+++ src/components/Layout/MobileMenu/MobileHeader.jsx
@@ -54,7 +54,7 @@ const MobileHeader = ({ open }) => {
                     <strong className="help float-center pr-5 pb-0 mb-0" style={{ fontSize: 18 + "px" }}>Need help placing an order ?</strong><br></br>
                     <span className="help-contact pt-0 mt-0"><i class="bi bi-telephone-outbound-fill pr-1 "><svg xmlns="http://www.w3.org/2000/svg" width="17" height="17" fill="currentColor" class="bi bi-telephone-outbound-fill" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511zM11 .5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V1.707l-4.146 4.147a.5.5 0 0 1-.708-.708L14.293 1H11.5a.5.5 0 0 1-.5-.5z" />
-                    </svg></i> <a href="tel:0751290264" style={{ color: "black", fontSize: 16 + "px" }}>+256 779 003322 </a></span>
+                    </svg></i> <a href="tel:0709744874" style={{ color: "black", fontSize: 16 + "px" }}>0709 744 874 </a></span>
                 </p>
                 <div className="px-2 d-flex justify-content-between">
                     <div className="d-flex justify-content-start mr-2">
